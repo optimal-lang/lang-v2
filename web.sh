3c68ab0980e705baccb32f974ebcb046835ecd9b
JavaCommons Technologies <javacommons@gmail.com>
2026-03-04 01:14:50 +0900

    # Commit at 2026.0304.0114.49 [DIFF] --- a/web.sh +++ b/web.sh     [FILE MODIFIED]     -start /d/PortableApps/GoogleChromePortable64/GoogleChromePortable.exe http://localhost:8080/ex-ball.html     +#chrome http://localhost:8080/ex-ball.html     +firefox http://localhost:8080/ex-ball.html [/DIFF]

--- web.sh
+++ web.sh
@@ -6,4 +6,5 @@ cwd=`pwd`
 ts=`date "+%Y.%m%d.%H%M.%S"`
 start python3 -m http.server 8080
 sleep 3
-start /d/PortableApps/GoogleChromePortable64/GoogleChromePortable.exe http://localhost:8080/ex-ball.html
+#chrome http://localhost:8080/ex-ball.html
+firefox http://localhost:8080/ex-ball.html
